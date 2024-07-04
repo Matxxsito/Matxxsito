3a05c26b5647ed554b424ff8340a4ff416b8e0b2
Jadde <jaddecoronelmarino@gmail.com>
2024-07-03 21:05:40 -0500

    primer comentario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { SobremiComponent } from './sobremi/sobremi.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, SobremiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
